59b2e86686fe43e48d7ede0011a275d8ed886c44
Wizaroo <64918538+Wizaroo@users.noreply.github.com>
2020-06-11 11:50:12 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// var form = document.getElementById('form');
+// form.submit () {
+// 	alert ('Заявка отправлена')
+// }
+// <form onsubmit="alert('submit!');return false">
+
+var btn = document.getElementById('btn-form');
+
+btn.onclick = function (e) {
+	// e.preventDefault();
+	alert ('Заявка отправлена');
+}
+
+
+$(function () {
+	$(window).scroll(function() {
+	    $('.icon1').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.icon2').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.icon3').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.form').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("rotateIn");
+	        }
+	    });
+	});
+})
